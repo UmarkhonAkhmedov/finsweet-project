4d75a9198cbd0dd09eb42d4bc056e9e31c1a5774
UmarkhonAkhmedov <76143018+UmarkhonAkhmedov@users.noreply.github.com>
2022-01-23 16:23:28 +0500

    Adding Elements of Navbar Component

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,20 +1,35 @@
 import Link from "next/link";
+import { signIn } from "next-auth/react"
 
+const navbarItems = ["home", "about-us", "features", "pricing", "faq", "blog"]
 function Navbar() {
   return (
-    <nav className="nav">
+    <nav className="navbar">
       <div className="container">
-        <div className="nav__logo">
-          <Link href="/">
-            <img src="/images/Home/website__logo.svg" alt="Website Logo"/>
-          </Link>
-        </div>
-        <div className="nav__main">
-          <div className="nav__list">
-
+        <div className="nav">
+          <div className="nav__logo">
+            <Link href="/">
+              <img src="/images/Home/website__logo.svg" alt="Website Logo"/>
+            </Link>
           </div>
-          <div className="nav__account">
-
+          <div className="nav__main">
+            <ul className="nav__list">
+              {navbarItems.map((item) => (
+                <li>
+                  <Link href={`/${item}`}>{item}</Link>
+                </li>
+              ))}
+            </ul>
+            {false ? (
+              <div>
+                Sign In
+              </div>
+            ) : (
+              <div className="nav__account">
+                <img src="https://avatars.githubusercontent.com/u/76143018?v=4" alt="Account Img"/>
+                <span>Umarkhon</span>
+              </div>
+            )}
           </div>
         </div>
       </div>
