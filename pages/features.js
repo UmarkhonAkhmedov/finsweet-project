72265f1567b719389c68589a8a6e2ed2719c8095
UmarkhonAkhmedov <76143018+UmarkhonAkhmedov@users.noreply.github.com>
2022-01-24 19:35:51 +0500

    Adding hero section for Features Page

---
+++ pages/features.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Hero from '../components/Home/Hero';
+
+function features() {
+  return (
+    <div>
+      <Hero heading="All the features you need" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." buttonMain="View Pricing" img="Features/hero__main.svg" link="/work" />
+    </div>
+  );
+}
+
+export default features;
