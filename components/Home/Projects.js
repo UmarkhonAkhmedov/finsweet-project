3ee6cd4179adb9c558c37c224ec2593dca7650ff
UmarkhonAkhmedov <76143018+UmarkhonAkhmedov@users.noreply.github.com>
2022-01-25 09:38:34 +0500

    Adding Main part of Project Section for Home Page

--- components/Home/Projects.js
+++ components/Home/Projects.js
@@ -12,8 +12,33 @@ function Projects({title, }) {
             <img src="/images/home/projects__icon-arrow.svg" alt=""/>
           </span>
         </div>
-        <div>
-
+        <div className='projects__main'>
+          <div className='projects__main--left'>
+            <img src="/images/home/project__main--large.png" alt=""/>
+            <div>
+              <h4>Workhub office Webflow Webflow Design</h4>
+              <p>Euismod faucibus turpis eu gravida mi. Pellentesque et velit aliquam </p>
+              <span>
+                <Link href="/work">View project</Link>
+                <img src="/images/home/projects__icon-arrow__yellow.svg" alt=""/>
+              </span>
+            </div>
+          </div>
+          <div className='projects__main--right'>
+            <div className='projects__main--right__top'>
+              <img src="/images/home/project__main--top.png" alt="" />
+              <div>
+                <h4>Unisaas Website Design</h4>
+                <span>
+                  <Link href="/work">View portfolio</Link>
+                  <img src="/images/home/projects__icon-arrow__yellow.svg" alt=""/>
+                </span>
+              </div>
+            </div>
+            <div className='projects__main--right__bottom'>
+              <img src="/images/home/project__main--bottom.png" alt="" />
+            </div>
+          </div>
         </div>
       </div>
     </div>
