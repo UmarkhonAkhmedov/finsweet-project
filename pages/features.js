48348965fe1e8dfe80bd16db174491b4baa56c25
UmarkhonAkhmedov <76143018+UmarkhonAkhmedov@users.noreply.github.com>
2022-01-28 00:01:11 +0500

    Adding Client Section for Features Page

--- pages/features.js
+++ pages/features.js
@@ -3,17 +3,20 @@ import Hero from '../components/Home/Hero';
 import Brand from '../components/Brand'
 import Feature from '../components/Feature';
 import { featuresAboutData } from '../components/dummyData';
+import InfoImg from '../components/InfoImg';
 
 function features() {
   return (
     <div>
       <Hero heading="All the features you need" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." buttonMain="View Pricing" img="Features/hero__main.svg" link="/pricing" />
       <Brand/>
-      <div className=''>
+      <div className='bottom-padding'>
         <h2 className='main__heading'>The benefits of working with our team</h2>
         <Feature data={featuresAboutData} color="#F4F6FC"/>
       </div>
-      
+      <div className='container'>
+        <InfoImg subTitle="Use Client-first" title="Top agencies and freelancers around the world use Client-first " text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation." img="/images/features/client__img.png"/>
+      </div>
     </div>
   );
 }
