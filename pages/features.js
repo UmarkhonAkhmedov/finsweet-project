51d886df10c456293d28b68d36e4c14d1d6962c1
UmarkhonAkhmedov <76143018+UmarkhonAkhmedov@users.noreply.github.com>
2022-01-28 00:02:29 +0500

    Adding FAQ Component for Features Page

--- pages/features.js
+++ pages/features.js
@@ -4,6 +4,7 @@ import Brand from '../components/Brand'
 import Feature from '../components/Feature';
 import { featuresAboutData } from '../components/dummyData';
 import InfoImg from '../components/InfoImg';
+import FAQ from '../components/FAQ'
 
 function features() {
   return (
@@ -17,6 +18,7 @@ function features() {
       <div className='container'>
         <InfoImg subTitle="Use Client-first" title="Top agencies and freelancers around the world use Client-first " text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation." img="/images/features/client__img.png"/>
       </div>
+      <FAQ/>
     </div>
   );
 }
