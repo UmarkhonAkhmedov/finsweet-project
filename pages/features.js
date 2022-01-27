4c6f1b0e44090424b4af84cd29d4045f310842cf
UmarkhonAkhmedov <76143018+UmarkhonAkhmedov@users.noreply.github.com>
2022-01-27 19:31:22 +0500

    Adding Feature Section for Features Page

--- pages/features.js
+++ pages/features.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import Hero from '../components/Home/Hero';
 import Brand from '../components/Brand'
+import Feature from '../components/Feature';
+import { featuresAboutData } from '../components/dummyData';
 
 function features() {
   return (
     <div>
       <Hero heading="All the features you need" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." buttonMain="View Pricing" img="Features/hero__main.svg" link="/pricing" />
       <Brand/>
+      <div className=''>
+        <h2 className='main__heading'>The benefits of working with our team</h2>
+        <Feature data={featuresAboutData} color="#F4F6FC"/>
+      </div>
+      
     </div>
   );
 }
