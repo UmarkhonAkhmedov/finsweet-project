5b417aa4495b5d4fa9a2ce516b2858ad39f401a4
UmarkhonAkhmedov <76143018+UmarkhonAkhmedov@users.noreply.github.com>
2022-01-27 16:03:34 +0500

    Adding FAQ Section for About Page

--- components/FAQ.js
+++ components/FAQ.js
@@ -1,32 +1,35 @@
+import Link from 'next/link';
 import React from 'react';
 import { Accordion, AccordionItem } from 'react-light-accordion';
-import 'react-light-accordion/demo/css/index.css';
 import { accordionData } from './dummyData';
 
 function FAQ() {
   return (
-    <div className='container'>
-      <div>
-
-      </div>
-      <div>
-        <Accordion atomic={true}>
-          <AccordionItem title={accordionData[0].title}>
-            <p>{accordionData[0].text}</p>
-          </AccordionItem>
-          <AccordionItem title={accordionData[1].title}>
-            <p>{accordionData[0].text}</p>
-          </AccordionItem>
-          <AccordionItem title={accordionData[2].title}>
-            <p>{accordionData[0].text}</p>
-          </AccordionItem>
-          <AccordionItem title={accordionData[3].title}>
-            <p>{accordionData[0].text}</p>
-          </AccordionItem>
-          <AccordionItem title={accordionData[4].title}>
-            <p>{accordionData[0].text}</p>
-          </AccordionItem>
-        </Accordion>
+    <div className='s-padding'>
+      <div className='container faq'>
+        <div className='faq__heading'>
+          <h2>Frequently asked questions</h2>
+          <Link href="/contact">Contact us for more info</Link>
+        </div>
+        <div className='faq__accordion'>
+          <Accordion atomic={true} className="accordion">
+            <AccordionItem className="accordion__title" title={accordionData[0].title}>
+              <p className='faq__accordion--text'>{accordionData[0].text}</p>
+            </AccordionItem>
+            <AccordionItem className="accordion__title" title={accordionData[1].title}>
+              <p className='faq__accordion--text'>{accordionData[0].text}</p>
+            </AccordionItem>
+            <AccordionItem className="accordion__title" title={accordionData[2].title}>
+              <p className='faq__accordion--text'>{accordionData[0].text}</p>
+            </AccordionItem>
+            <AccordionItem className="accordion__title" title={accordionData[3].title}>
+              <p className='faq__accordion--text'>{accordionData[0].text}</p>
+            </AccordionItem>
+            <AccordionItem className="accordion__title" title={accordionData[4].title}>
+              <p className='faq__accordion--text'>{accordionData[0].text}</p>
+            </AccordionItem>
+          </Accordion>
+        </div>
       </div>
     </div>
   )
