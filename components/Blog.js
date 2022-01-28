87c835cfa59252752ec743980e708daca3500ea3
UmarkhonAkhmedov <76143018+UmarkhonAkhmedov@users.noreply.github.com>
2022-01-28 05:35:09 +0500

    Adding Blog Section for Blog Page

--- components/Blog.js
+++ components/Blog.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import Link from 'next/link';
 
-function Blog() {
-  return <div></div>;
+function Blog({data}) {
+  return (
+    <div className='container'>
+      <div className='blog'>
+        {data.map((index, id) => (
+          <div className='blog__block' key={id}>
+            <img className='blog__block--img' src={`/images/blog/blog__${index.img}.png`}/>
+            <time>{index.date}</time>
+            <h6>{index.title}</h6>
+            <p>{index.text}</p>
+            <Link href="/blog:id">
+              <span>Read More <img src="/images/blog/blog__arrow.svg" alt="" /></span>
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Blog;
--- pages/blog.js
+++ pages/blog.js
@@ -1,9 +1,13 @@
+import Blog from '../components/Blog';
 import Article from '../components/Blog/Article';
+import { BlogblogData } from '../components/dummyData';
 
 function blog() {
   return (
     <div>
       <Article/>
+      <h2 style={{textAlign: "center"}}>Our Blog</h2>
+      <Blog data={BlogblogData}/>
     </div>
   );
 }
