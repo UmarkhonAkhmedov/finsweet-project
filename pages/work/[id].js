b0396ffb7141ff716405713afd72462ee15fa094
UmarkhonAkhmedov <76143018+UmarkhonAkhmedov@users.noreply.github.com>
2022-01-29 03:01:16 +0500

    Adding Details Component for Case Studies Section

--- pages/work/[id].js
+++ pages/work/[id].js
@@ -1,14 +1,35 @@
 import React from 'react';
 
-function Details() {
+function CaseStudies() {
   return (
-    <div className='s-padding'>
-      <h1>Details</h1>
+    <div className='container'>
+      <div className='caseStudies'>
+        <h4>Web design and development</h4>
+        <h2>Finsweet Design case studies</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse.</p>
+        <img className='caseStudies__img' src="/images/work/work__id--main.png" alt="" />
+        <div className='caseStudies__details'>
+          <div>
+            <h6>Client</h6>
+            <h5>facebook.com</h5>
+          </div>
+          <div>
+            <h6>Service</h6>
+            <h5>Product Design</h5>
+          </div>
+          <div>
+            <h6>Deliverable</h6>
+            <h5>UI Screens, UX Flow & Prototype</h5>
+          </div>
+        </div>
+
+      
+      </div>
     </div>
   );
 }
 
-export default Details;
+export default CaseStudies;
 
 
 
