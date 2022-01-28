c3d0d71edb45cd554a342a44d74a462431b7dd42
UmarkhonAkhmedov <76143018+UmarkhonAkhmedov@users.noreply.github.com>
2022-01-29 03:23:51 +0500

    Adding About Component for Case Studies Page

--- pages/work/[id].js
+++ pages/work/[id].js
@@ -23,7 +23,17 @@ function CaseStudies() {
           </div>
         </div>
 
-      
+        <div className='caseStudies__about'>
+          <h3>About the project</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <ul>
+            <li><img src="/images/work/work__id--about__circle.svg"/> Lorem ipsum dolor sit amet, consectetur adipiscing elit</li>
+            <li><img src="/images/work/work__id--about__circle.svg"/> Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea</li>
+            <li><img src="/images/work/work__id--about__circle.svg"/> Duis aute irure dolor in reprehenderit in voluptate velit esse</li>
+            <li><img src="/images/work/work__id--about__circle.svg"/> Excepteur sint occaecat cupidatat non proident, sunt in culpa</li>
+          </ul>
+          <img className='caseStudies__about--img' src="/images/work/work__id--about.png"/>
+        </div>
       </div>
     </div>
   );
