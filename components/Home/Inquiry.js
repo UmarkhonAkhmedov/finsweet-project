5290177dec94bc1890cf9ccfe2c384ae1f5a7786
Umarkhon <umarkhonakhmedov2000@gmail.com>
2022-05-30 01:55:27 +0500

    Inquiry img problem

--- components/Home/Inquiry.js
+++ components/Home/Inquiry.js
@@ -21,8 +21,12 @@ function Inquiry() {
               <button>Send an Inquiry</button>
             </form>
             <div className='inquiry__contact--details__link'>
-              <Link href="/contact" passHref>Get in touch with us</Link>
-              <Image width={24} height={15} src="/images/Home/send__arrow.svg" alt=""/>
+              <div>
+                <Link href="/contact" passHref>Get in touch with us</Link>
+              </div>
+              <div className='inquiry__contact--details__link--img'>
+                <Image width={24} height={15} src="/images/Home/send__arrow.svg" alt=""/>
+              </div>
             </div>
           </div>
         </div>  
